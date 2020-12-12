dcf757676cdbe9dd3dd2751710b1269ffea0bb2d
maps <ycorrale@cern.ch>
2020-12-11 21:43:12 -0700

    ycm - Update to new register address

--- include/FELIXwrapper/registers.h
+++ include/FELIXwrapper/registers.h
@@ -12,12 +12,12 @@ uint32_t SC_LINK = 0xe100;
 uint32_t SC_RESET = 0xe110;
 uint32_t SWT_WR_WORD_L = 0xe160;
 uint32_t SWT_WR_WORD_M = 0xe164;
-uint32_t SWT_WR_WORD_H = 0xe168;
-uint32_t SWT_MON = 0xe190;
-uint32_t SWT_CMD = 0xe170;
-uint32_t SWT_RD_WORD_L = 0xe180;
-uint32_t SWT_RD_WORD_M = 0xe184;
-uint32_t SWT_RD_WORD_H = 0xe188;
+uint32_t SWT_WR_WORD_H = 0xe170;
+uint32_t SWT_MON = 0xe1b0;
+uint32_t SWT_CMD = 0xe180;
+uint32_t SWT_RD_WORD_L = 0xe190;
+uint32_t SWT_RD_WORD_M = 0xe194;
+uint32_t SWT_RD_WORD_H = 0xe1a0;
 
 } // namespace ScRegs
 } // namespace Cru
